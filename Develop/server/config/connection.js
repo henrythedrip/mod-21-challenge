3c97abf629d583e17a1aadd2194d8084711c5106
henrythedrip <146283648+henrythedrip@users.noreply.github.com>
2024-04-10 11:19:45 -0600

    changed my process.env.mongodb_uri enviornment variable in connection.js

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://henrythomas123:KakyVAtOJ3HUUuDH@cluster0.pjfuqtz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 module.exports = mongoose.connection;
